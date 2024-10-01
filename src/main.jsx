cf66e414f3fb81dfdccbaed4791ee35eb874317e
F2RUZ <gaffarovferuz4@gmail.com>
2024-10-01 07:36:32 -0500

    login

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </GoogleOAuthProvider>
+  <Router>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </GoogleOAuthProvider>
+  </Router>
 );
